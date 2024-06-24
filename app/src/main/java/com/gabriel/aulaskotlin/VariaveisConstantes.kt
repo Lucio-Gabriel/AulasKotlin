f4fc05fecde3ed0682994ab339a0158953a9c7fc
Gabriel <suppershock777@gmail.com>
2024-06-24 09:23:11 -0300

    "Aqui ficaram todas as aulas do kotlin"

---
+++ app/src/main/java/com/gabriel/aulaskotlin/VariaveisConstantes.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var nome: String = "João"
+    nome = "Maria"
+    println(nome)
+    println("########################")
+
+
+    val cidade: String = "Sao paulo"
+    print(cidade)
+    println("########################")
+
+    val a: Int = 10
+    val b: Int = 20
+    val soma: Int = a + b
+
+    println("a soma é: " + soma)
+
+}
+
